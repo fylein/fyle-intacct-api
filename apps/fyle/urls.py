7c1db9355584bd90fea3c71f044bf30cd6efc170
ashwin1111 <winash1998@gmail.com>
2020-07-21 20:44:17 +0530

    Feature: Fyle models / urls / views

---
+++ apps/fyle/urls.py
@@ -0,0 +1,30 @@
+"""fyle_intacct_api URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from .views import UserProfileView, ExpenseGroupView, ExpenseGroupScheduleView, ExpenseGroupByIdView, \
+    ExpenseView, EmployeeView, CategoryView, ProjectView
+
+urlpatterns = [
+    path('user/', UserProfileView.as_view()),
+    path('expense_groups/', ExpenseGroupView.as_view()),
+    path('expense_groups/trigger/', ExpenseGroupScheduleView.as_view()),
+    path('expense_groups/<int:expense_group_id>/', ExpenseGroupByIdView.as_view()),
+    path('expense_groups/<int:expense_group_id>/expenses/', ExpenseView.as_view()),
+    path('employees/', EmployeeView.as_view()),
+    path('categories/', CategoryView.as_view()),
+    path('projects/', ProjectView.as_view())
+]
---
+++ apps/fyle/utils.py
@@ -0,0 +1,223 @@
+from typing import List
+import json
+
+from django.conf import settings
+
+from fylesdk import FyleSDK, UnauthorizedClientError, NotFoundClientError, InternalServerError, WrongParamsError
+
+from fyle_accounting_mappings.models import ExpenseAttribute
+
+import requests
+
+class FyleConnector:
+    """
+    Fyle utility functions
+    """
+
+    def __init__(self, refresh_token, workspace_id=None):
+        client_id = settings.FYLE_CLIENT_ID
+        client_secret = settings.FYLE_CLIENT_SECRET
+        base_url = settings.FYLE_BASE_URL
+        self.workspace_id = workspace_id
+
+        self.connection = FyleSDK(
+            base_url=base_url,
+            client_id=client_id,
+            client_secret=client_secret,
+            refresh_token=refresh_token,
+            jobs_url=settings.FYLE_JOBS_URL
+        )
+
+    def _post_request(self, url, body):
+        """
+        Create a HTTP post request.
+        """
+
+        access_token = self.connection.access_token
+        api_headers = {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer {0}'.format(access_token)
+        }
+
+        response = requests.post(
+            url,
+            headers=api_headers,
+            data=body
+        )
+
+        if response.status_code == 200:
+            return json.loads(response.text)
+
+        elif response.status_code == 401:
+            raise UnauthorizedClientError('Wrong client secret or/and refresh token', response.text)
+
+        elif response.status_code == 404:
+            raise NotFoundClientError('Client ID doesn\'t exist', response.text)
+
+        elif response.status_code == 400:
+            raise WrongParamsError('Some of the parameters were wrong', response.text)
+
+        elif response.status_code == 500:
+            raise InternalServerError('Internal server error', response.text)
+
+    def _get_request(self, url, params):
+        """
+        Create a HTTP get request.
+        """
+
+        access_token = self.connection.access_token
+        api_headers = {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer {0}'.format(access_token)
+        }
+        api_params = {}
+
+        for k in params:
+            # ignore all unused params
+            if not params[k] is None:
+                p = params[k]
+
+                # convert boolean to lowercase string
+                if isinstance(p, bool):
+                    p = str(p).lower()
+
+                api_params[k] = p
+
+        response = requests.get(
+            url,
+            headers=api_headers,
+            params=api_params
+        )
+
+        if response.status_code == 200:
+            return json.loads(response.text)
+
+        elif response.status_code == 401:
+            raise UnauthorizedClientError('Wrong client secret or/and refresh token', response.text)
+
+        elif response.status_code == 404:
+            raise NotFoundClientError('Client ID doesn\'t exist', response.text)
+
+        elif response.status_code == 400:
+            raise WrongParamsError('Some of the parameters were wrong', response.text)
+
+        elif response.status_code == 500:
+            raise InternalServerError('Internal server error', response.text)
+
+    def get_employee_profile(self):
+        """
+        Get expenses from fyle
+        """
+        employee_profile = self.connection.Employees.get_my_profile()
+
+        return employee_profile['data']
+
+    def get_cluster_domain(self):
+        """
+        Get cluster domain name from fyle
+        """
+
+        body = {}
+        api_url = '{0}/oauth/cluster/'.format(settings.FYLE_BASE_URL)
+
+        return self._post_request(api_url, body)
+
+    def get_fyle_orgs(self, cluser_domain):
+        """
+        Get fyle orgs of a user
+        """
+
+        params = {}
+        api_url = '{0}/api/orgs/'.format(cluser_domain)
+
+        return self._get_request(api_url, params)
+
+    def get_expenses(self, state: List[str], updated_at: List[str]):
+        """
+        Get expenses from fyle
+        """
+        expenses = self.connection.Expenses.get_all(state=state, updated_at=updated_at)
+        expenses = list(
+            filter(lambda expense: expense['reimbursable'], expenses))
+        return expenses
+
+    def sync_employees(self):
+        """
+        Get employees from fyle
+        """
+        employees = self.connection.Employees.get_all()
+
+        employee_attributes = []
+
+        for employee in employees:
+            employee_attributes.append({
+                'attribute_type': 'EMPLOYEE',
+                'display_name': 'Employee',
+                'value': employee['employee_email'],
+                'source_id': employee['id']
+            })
+
+        employee_attributes = ExpenseAttribute.bulk_upsert_expense_attributes(employee_attributes, self.workspace_id)
+
+        return employee_attributes
+
+    def sync_categories(self, active_only: bool):
+        """
+        Get categories from fyle
+        """
+        categories = self.connection.Categories.get(active_only=active_only)['data']
+
+        category_attributes = []
+
+        for category in categories:
+            if category['name'] != category['sub_category']:
+                category['name'] = '{0} / {1}'.format(category['name'], category['sub_category'])
+
+            category_attributes.append({
+                'attribute_type': 'CATEGORY',
+                'display_name': 'Category',
+                'value': category['name'],
+                'source_id': category['id']
+            })
+
+        category_attributes = ExpenseAttribute.bulk_upsert_expense_attributes(category_attributes, self.workspace_id)
+
+        return category_attributes
+
+    def sync_projects(self, active_only: bool):
+        """
+        Get projects from fyle
+        """
+        projects = self.connection.Projects.get(active_only=active_only)['data']
+
+        project_attributes = []
+
+        for project in projects:
+            project_attributes.append({
+                'attribute_type': 'PROJECT',
+                'display_name': 'Project',
+                'value': project['name'],
+                'source_id': project['id']
+            })
+
+        project_attributes = ExpenseAttribute.bulk_upsert_expense_attributes(project_attributes, self.workspace_id)
+
+        return project_attributes
+
+    def get_attachments(self, expense_ids: List[str], expense_numbers: List[str]):
+        """
+        Get attachments against expense_ids
+        """
+        attachments = []
+        if expense_ids:
+            index = 0
+            for expense_id in expense_ids:
+                attachment = self.connection.Expenses.get_attachments(expense_id)
+                if attachment['data']:
+                    attachment = attachment['data'][0]
+                    attachment['expense_number'] = expense_numbers[index]
+                    attachments.append(attachment)
+                    index = index + 1
+            return attachments
+
+        return []
