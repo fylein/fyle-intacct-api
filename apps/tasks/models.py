7cf5516b3df6444aa5aed75a9c0945cc2472fc96
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2020-07-23 12:21:09 +0530

    Feature: Tasks models / urls / views

---
+++ apps/tasks/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+from apps.workspaces.models import Workspace
+from apps.fyle.models import ExpenseGroup
+from apps.sage_intacct.models import VendorBill, ExpenseReport
+
+
+def get_default():
+    return {
+        'default': 'default value'
+    }
+
+
+class TaskLog(models.Model):
+    """
+    Table to store task logs
+    """
+    id = models.AutoField(primary_key=True)
+    workspace = models.ForeignKey(Workspace, on_delete=models.PROTECT, help_text='Reference to Workspace model')
+    type = models.CharField(max_length=50, help_text='Task type (FETCH_EXPENSES / CREATE_BILL / CREATE_EXPENSE)')
+    task_id = models.CharField(max_length=255, null=True, help_text='Fyle Jobs task reference')
+    expense_group = models.ForeignKey(ExpenseGroup, on_delete=models.PROTECT,
+                                      null=True, help_text='Reference to Expense group')
+    vendor_bill = models.ForeignKey(VendorBill, on_delete=models.PROTECT, \
+        help_text='Reference to VendorBill', null=True)
+    expense_report = models.ForeignKey(ExpenseReport, on_delete=models.PROTECT, \
+        help_text='Reference to ExpenseReport', null=True)
+    status = models.CharField(max_length=255, help_text='Task Status')
+    detail = JSONField(help_text='Task Response', null=True, default=get_default)
+    created_at = models.DateTimeField(auto_now_add=True, help_text='Created at datetime')
+    updated_at = models.DateTimeField(auto_now=True, help_text='Updated at datetime')
+
+    class Meta:
+        db_table = 'task_log'
