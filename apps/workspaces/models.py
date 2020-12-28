ba482ad285547b0ae0d41362c1fd037ae5c6c151
ashwinthanaraj <ashwin.t@fyle.in>
2020-12-28 13:35:57 +0530

    resolve comments

--- apps/workspaces/models.py
+++ apps/workspaces/models.py
@@ -38,7 +38,7 @@ class WorkspaceGeneralSettings(models.Model):
     updated_at = models.DateTimeField(auto_now=True, help_text='Updated at')
 
     class Meta:
-        db_table = 'general_settings'
+        db_table = 'workspace_general_settings'
 
 
 class SageIntacctCredential(models.Model):
