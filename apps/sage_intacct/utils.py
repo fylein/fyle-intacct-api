3b57bd5ac45968b577f8a378b30313319081a9c6
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-02-25 16:58:45 +0530

    Send currency in payload (#36)

--- apps/sage_intacct/utils.py
+++ apps/sage_intacct/utils.py
@@ -321,6 +321,7 @@ class SageIntacctConnector:
             },
             'state': 'Submitted',
             'description': expense_report.memo,
+            'currency': expense_report.currency,
             'expenses': {
                 'expense': expsense_payload
             }
@@ -360,6 +361,7 @@ class SageIntacctConnector:
             'VENDORID': bill.vendor_id,
             'RECORDID': bill.memo,
             'WHENDUE': current_date,
+            'CURRENCY': bill.currency,
             'APBILLITEMS': {
                 'APBILLITEM': bill_lineitems_payload
             }
@@ -400,6 +402,7 @@ class SageIntacctConnector:
                 'day': transaction_date.day
             },
             'description': charge_card_transaction.memo,
+            'currency': charge_card_transaction.currency,
             'ccpayitems': {
                 'ccpayitem': charge_card_transaction_payload
             }
