eea93f6b28c823e899ff5865548b500c90e4acc2
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2021-03-04 12:31:36 +0530

    Auto create employee/ vendor in SageIntacct

--- apps/workspaces/models.py
+++ apps/workspaces/models.py
@@ -42,6 +42,7 @@ class WorkspaceGeneralSettings(models.Model):
                                                                                       'Intacct to Fyle')
     auto_map_employees = models.CharField(max_length=50,
                                          help_text='Auto Map Employees type from Sage Intacct to Fyle', null=True)
+    auto_create_destination_entity = models.BooleanField(default=False, help_text='Auto create vendor / employee')
     created_at = models.DateTimeField(auto_now_add=True, help_text='Created at')
     updated_at = models.DateTimeField(auto_now=True, help_text='Updated at')
 
