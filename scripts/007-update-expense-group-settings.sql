8e3ff6b1041dccb1995be8e64e01dcc2c52b6da4
ashwinthanaraj <ashwin.t@fyle.in>
2021-04-08 20:23:38 +0530

    query changes

--- scripts/007-update-expense-group-settings.sql
+++ scripts/007-update-expense-group-settings.sql
@@ -2,4 +2,9 @@ rollback;
 begin;
 
 update expense_group_settings 
-set corporate_credit_card_expense_group_fields = corporate_credit_card_expense_group_fields || '{"expense_id"}';
+set corporate_credit_card_expense_group_fields = corporate_credit_card_expense_group_fields || '{"expense_id"}' 
+where workspace_id in (
+    select workspace_id from workspace_general_settings where 
+    corporate_credit_card_expenses_object = 'CHARGE_CARD_TRANSACTION'
+)
+;
