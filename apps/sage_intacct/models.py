54bacc3a04a007d368f33c14d8213ea759d1d3b5
Shwetabh Kumar <shwetabh002@gmail.com>
2021-10-14 13:04:41 +0530

    Fix: Expense Group fund source instead of generated strinf

--- apps/sage_intacct/models.py
+++ apps/sage_intacct/models.py
@@ -238,7 +238,7 @@ def get_memo(expense_group: ExpenseGroup, payment_type: str=None) -> str:
         expense_group_settings: ExpenseGroupSettings = ExpenseGroupSettings.objects.get(
             workspace_id=expense_group.workspace_id
         )
-        if expense_fund_source == 'CCC':
+        if expense_group.fund_source == 'CCC':
             if expense_group_settings.ccc_export_date_type != 'current_date':
                 date = get_transaction_date(expense_group)
                 date = (datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')).strftime('%d/%m/%Y')
