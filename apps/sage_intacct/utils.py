bd478d82d1c0ed30513ebaef674289f497113e15
harshithaputtaswamy <44926484+harshithaputtaswamy@users.noreply.github.com>
2022-03-09 12:03:47 +0530

    sync only employess whose status is active for sage intacct (#138)
    Co-authored-by: harshithaputtaswamy <harshithaputtaswamy>

--- apps/sage_intacct/utils.py
+++ apps/sage_intacct/utils.py
@@ -335,7 +335,7 @@ class SageIntacctConnector:
         """
         Get employees
         """
-        employees = self.connection.employees.get_all()
+        employees = self.connection.employees.get_all(field='STATUS', value='active')
 
         employee_attributes = []
 
