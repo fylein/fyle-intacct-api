24b993ee428b1b013ecdedf97b09a5e888143235
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-05-21 09:16:58 +0530

    Export CCC refunds for expense reports config

--- apps/workspaces/signals.py
+++ apps/workspaces/signals.py
@@ -9,6 +9,7 @@ from apps.sage_intacct.helpers import schedule_payment_sync
 from apps.mappings.helpers import schedule_or_delete_auto_mapping_tasks
 
 from .models import Configuration
+from ..fyle.models import ExpenseGroupSettings
 
 
 @receiver(post_save, sender=Configuration)
@@ -18,6 +19,11 @@ def run_post_configration_triggers(sender, instance: Configuration, **kwargs):
     :param instance: Row Instance of Sender Class
     :return: None
     """
+    if instance.corporate_credit_card_expenses_object == 'EXPENSE_REPORT':
+        expense_group_settings = ExpenseGroupSettings.objects.get(workspace_id=int(instance.workspace_id))
+        expense_group_settings.import_card_credits = True
+        expense_group_settings.save()
+
     if instance.corporate_credit_card_expenses_object == 'CHARGE_CARD_TRANSACTION':
         add_expense_id_to_expense_group_settings(int(instance.workspace_id))
 
