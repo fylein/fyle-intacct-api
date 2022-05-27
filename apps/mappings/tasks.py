d5717f7fcb03730b7e5b60e0cb0644ecc79a0ec0
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2022-05-27 12:41:04 +0530

    Fix removing of duplicate as case insensitive (#171)

--- apps/mappings/tasks.py
+++ apps/mappings/tasks.py
@@ -27,9 +27,9 @@ def remove_duplicates(si_attributes: List[DestinationAttribute]):
     attribute_values = []
 
     for attribute in si_attributes:
-        if attribute.value not in attribute_values:
+        if attribute.value.lower() not in attribute_values:
             unique_attributes.append(attribute)
-            attribute_values.append(attribute.value)
+            attribute_values.append(attribute.value.lower())
 
     return unique_attributes
 
