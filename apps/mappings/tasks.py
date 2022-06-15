8b64a90a0c1a7b78b377c740688a840dcaa5cb71
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2022-06-15 11:14:04 +0530

    Changes to updated existing categories (#175)

--- apps/mappings/tasks.py
+++ apps/mappings/tasks.py
@@ -228,7 +228,27 @@ def schedule_auto_map_charge_card_employees(workspace_id: int):
             schedule.delete()
 
 
-def create_fyle_categories_payload(categories: List[DestinationAttribute], workspace_id: int):
+def get_all_categories_from_fyle(platform: PlatformConnector):
+    categories_generator = platform.connection.v1beta.admin.categories.list_all(query_params={
+            'order': 'id.desc'
+        })
+
+    categories = []
+
+    for response in categories_generator:
+        if response.get('data'):
+            categories.extend(response['data'])
+
+    category_name_map = {}
+    for category in categories:
+        if category['sub_category'] and category['name'] != category['sub_category']:
+                    category['name'] = '{0} / {1}'.format(category['name'], category['sub_category'])
+        category_name_map[category['name'].lower()] = category
+
+    return category_name_map
+
+
+def create_fyle_categories_payload(categories: List[DestinationAttribute], workspace_id: int, category_map: Dict):
     """
     Create Fyle Categories Payload from Sage Intacct Expense Types / Accounts
     :param workspace_id: Workspace integer id
@@ -237,15 +257,20 @@ def create_fyle_categories_payload(categories: List[DestinationAttribute], works
     """
     payload = []
 
-    existing_category_names = ExpenseAttribute.objects.filter(
-        attribute_type='CATEGORY', workspace_id=workspace_id).values_list('value', flat=True)
-
     for category in categories:
-        if category.value not in existing_category_names:
+        if category.value.lower() not in category_map:
             payload.append({
                 'name': category.value,
                 'code': category.destination_id,
                 'is_enabled': True if category.active is None else category.active,
+                'restricted_project_ids': None
+            })
+        else:
+            payload.append({
+                'id': category_map[category.value.lower()]['id'],
+                'name': category.value,
+                'code': category.destination_id,
+                'is_enabled': category_map[category.value.lower()]['is_enabled'],
                 'restricted_project_ids': None
             })
 
@@ -537,6 +562,8 @@ def upload_categories_to_fyle(workspace_id: int, reimbursable_expenses_object: s
 
     platform = PlatformConnector(fyle_credentials)
 
+    category_map = get_all_categories_from_fyle(platform=platform)
+
     si_connection = SageIntacctConnector(
         credentials_object=si_credentials,
         workspace_id=workspace_id
@@ -556,7 +583,7 @@ def upload_categories_to_fyle(workspace_id: int, reimbursable_expenses_object: s
 
     si_attributes = remove_duplicates(si_attributes)
 
-    fyle_payload: List[Dict] = create_fyle_categories_payload(si_attributes, workspace_id)
+    fyle_payload: List[Dict] = create_fyle_categories_payload(si_attributes, workspace_id, category_map)
     if fyle_payload:
         platform.categories.post_bulk(fyle_payload)
         platform.categories.sync()
