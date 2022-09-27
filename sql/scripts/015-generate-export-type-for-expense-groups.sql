2cd113d955eeb094bf1924c62328ccab0d11b120
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-09-27 19:51:10 +0530

    fux

--- sql/scripts/015-generate-export-type-for-expense-groups.sql
+++ sql/scripts/015-generate-export-type-for-expense-groups.sql
@@ -3,7 +3,7 @@
 rollback;
 begin;
 
-update expense_groups set export_type = 'EXPENSE REPORT' where id in (
+update expense_groups set export_type = 'EXPENSE_REPORT' where id in (
         select expense_group_id from task_logs where type = 'CREATING_EXPENSE_REPORTS' and status = 'COMPLETE' and expense_report_id is not null
     );
 
@@ -11,11 +11,11 @@ update expense_groups set export_type = 'BILL' where id in (
         select expense_group_id from task_logs where type = 'CREATING_BILLS' and status = 'COMPLETE' and bill_id is not null
     );
 
-update expense_groups set export_type = 'JOURNAL ENTRY' where id in (
+update expense_groups set export_type = 'JOURNAL_ENTRY' where id in (
         select expense_group_id from task_logs where type = 'CREATING_JOURNAL_ENTRIES' and status = 'COMPLETE' and journal_entry_id is not null
     );
 
-update expense_groups set export_type = 'CHARGE CARD TRANSACTION' where id in (
+update expense_groups set export_type = 'CHARGE_CARD_TRANSACTION' where id in (
         select expense_group_id from task_logs where type = 'CREATING_CHARGE_CARD_TRANSACTIONS' and status = 'COMPLETE' and charge_card_transaction_id is not null
     );
 
