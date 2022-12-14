40492587431d9f31d079491577f3bbcb5d50459d
harshithaputtaswamy <harshitha.p@fyle.in>
2022-12-14 12:31:01 +0530

    fixed comments

--- tests/test_fyle/test_helpers.py
+++ tests/test_fyle/test_helpers.py
@@ -8,15 +8,11 @@ from apps.fyle.helpers import post_request, get_request, get_fyle_orgs
 def test_post_request(mocker):
     mocker.patch(
         'apps.fyle.helpers.requests.post',
-        return_value=Response(
-            {
-                'message': 'Post request'
-            },
-            status=status.HTTP_200_OK
-        )
+        return_value=mock.MagicMock(status_code=200, text="{'key': 'dfghjk'}")
     )
     try:
-        post_request(url='sdfghjk', body={}, refresh_token='srtyu')
+        response = post_request(url='sdfghjk', body={}, refresh_token='srtyu')
+        assert response == {'key': 'dfghjk'}
     except:
         logger.info('Error in post request')
     
@@ -38,15 +34,11 @@ def test_post_request(mocker):
 def test_get_request(mocker):
     mocker.patch(
         'apps.fyle.helpers.requests.get',
-        return_value=Response(
-            {
-                'message': 'Get request'
-            },
-            status=status.HTTP_200_OK
-        )
+        return_value=mock.MagicMock(status_code=200, text="{'key': 'dfghjk'}")
     )
     try:
-        get_request(url='sdfghjk', params={'key': 'dfghjk'}, refresh_token='srtyu')
+        response = get_request(url='sdfghjk', params={'key': 'dfghjk'}, refresh_token='srtyu')
+        assert response == {'key': 'dfghjk'}
     except:
         logger.info('Error in post request')
     
@@ -68,14 +60,10 @@ def test_get_request(mocker):
 def test_get_fyle_orgs(mocker):
     mocker.patch(
         'apps.fyle.helpers.requests.get',
-        return_value=Response(
-            {
-                'message': 'Get request'
-            },
-            status=status.HTTP_200_OK
-        )
+        return_value=mock.MagicMock(status_code=200, text="{'orgs': 'dfghjk'}")
     )
     try:
-        get_fyle_orgs(refresh_token='srtyu', cluster_domain='erty')
+        response = get_fyle_orgs(refresh_token='srtyu', cluster_domain='erty')
+        assert response == {'orgs': 'dfghjk'}
     except:
         logger.info('Error in post request')
