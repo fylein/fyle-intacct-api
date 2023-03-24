5ce323a62a08ded28e98a66ecd01ef29dfb2c112
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2023-03-24 16:26:36 +0530

    Invalid token, Invalid Credentials, No attribute Date, Local Variable… (#251)
    * Invalid token, Invalid Credentials, No attribute Date, Local Variable Admin name errors resolved
    
    * Comments Resolved
    
    * Exceptions added to mapping/task.py
    
    * Comments resolved
    
    * Docker file updated

--- apps/workspaces/tasks.py
+++ apps/workspaces/tasks.py
@@ -1,4 +1,5 @@
 import time
+import logging
 from datetime import datetime, timedelta, date
 from typing import List
 
@@ -16,6 +17,9 @@ from apps.sage_intacct.tasks import schedule_expense_reports_creation, schedule_
 from apps.tasks.models import TaskLog
 from apps.workspaces.models import User, Workspace, WorkspaceSchedule, Configuration, SageIntacctCredential, FyleCredential
 
+
+logger = logging.getLogger(__name__)
+logger.level = logging.INFO
 
 def schedule_email_notification(workspace_id: int, schedule_enabled: bool, hours: int):
     if schedule_enabled:
@@ -149,18 +153,28 @@ def run_email_notification(workspace_id):
 
     workspace = Workspace.objects.get(id=workspace_id)
     admin_data = WorkspaceSchedule.objects.get(workspace_id=workspace_id)
-    intacct = SageIntacctCredential.objects.get(workspace=workspace)
+    try:
+        intacct = SageIntacctCredential.objects.get(workspace=workspace)
+    except SageIntacctCredential.DoesNotExist:
+        logger.info('SageIntacct Credentials does not exist - %s', workspace_id)
+        return
 
     if ws_schedule.enabled:
         for admin_email in admin_data.emails_selected:
             attribute = ExpenseAttribute.objects.filter(workspace_id=workspace_id, value=admin_email).first()
-
+            
+            admin_name = 'Admin'
             if attribute:
                 admin_name = attribute.detail['full_name']
             else:
                 for data in admin_data.additional_email_options:
                     if data['email'] == admin_email:
                         admin_name = data['name']
+
+            if workspace.last_synced_at and workspace.ccc_last_synced_at:
+                export_time = max(workspace.last_synced_at, workspace.ccc_last_synced_at)
+            else:
+                export_time =  workspace.last_synced_at or workspace.ccc_last_synced_at
 
             if task_logs and (ws_schedule.error_count is None or len(task_logs) > ws_schedule.error_count):
                 context = {
@@ -169,7 +183,7 @@ def run_email_notification(workspace_id):
                     'fyle_company': workspace.name,
                     'intacct_company': intacct.si_company_name,
                     'workspace_id': workspace_id,
-                    'export_time': workspace.last_synced_at.date(),
+                    'export_time': export_time.date(),
                     'year': date.today().year,
                     'app_url': "{0}/workspaces/{1}/expense_groups".format(settings.FYLE_APP_URL, workspace_id)
                     }
