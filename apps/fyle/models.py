1173326a78f69c569f0cb1f7ee7afcbcbea50ac3
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2023-05-16 15:36:34 +0530

    Alloweded fields added (#286)

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -18,7 +18,7 @@ from apps.workspaces.models import Workspace
 ALLOWED_FIELDS = [
     'employee_email', 'report_id', 'claim_number', 'settlement_id',
     'fund_source', 'vendor', 'category', 'project', 'cost_center',
-    'verified_at', 'approved_at', 'spent_at', 'expense_id'
+    'verified_at', 'approved_at', 'spent_at', 'expense_id', 'expense_number', 'payment_number'
 ]
 
 ALLOWED_FORM_INPUT = {
