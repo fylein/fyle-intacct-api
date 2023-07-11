91543fe806f18499385cb0cdafb0cc6e5a337841
Nilesh Pant <nileshpant6969@gmail.com>
2023-07-11 14:28:09 +0530

    add tests;

--- tests/test_tasks/test_views.py
+++ tests/test_tasks/test_views.py
@@ -14,6 +14,21 @@ def test_get_queryset(api_client, test_connection):
     assert response.status_code==200
 
 
+def test_new_task_get_queryset(api_client, test_connection):
+    workspace_id = 1
+
+    access_token = test_connection.access_token
+    url = '/api/workspaces/{}/tasks/v2/all/'.format(workspace_id)
+
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer {}'.format(access_token))
+
+    response = api_client.get(url, {
+        'expense_group_ids': '4',
+        'task_type': 'CREATING_EXPENSE',
+        'status': 'ALL'
+    })
+    assert response.status_code==200
+
 def test_get_task_by_id(api_client, test_connection):
     workspace_id = 1
     
