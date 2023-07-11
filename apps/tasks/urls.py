7a0ba196fc5002a3b2146f8c3ac226598b087c61
Nilesh Pant <nileshpant6969@gmail.com>
2023-07-11 14:37:34 +0530

    minor change

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('', TasksByIdView.as_view()),
     path('expense_group/<int:expense_group_id>/', TasksByExpenseGroupIdView.as_view()),
     path('all/', TasksView.as_view()),
-    path('/v2/all/', NewTaskView)
+    path('/v2/all/', NewTaskView.as_view())
 ]
