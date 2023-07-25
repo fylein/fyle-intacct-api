2a7709719eeb06bbc7fe9bb65e4ac56c13599c86
Fyle <fyle@Fyles-MacBook-Air.local>
2023-07-25 18:34:58 +0530

    Checkbox field support to skip exports

--- tests/test_fyle/test_helpers.py
+++ tests/test_fyle/test_helpers.py
@@ -372,6 +372,23 @@ def test_construct_expense_filter():
 
     assert constructed_expense_filter == response
 
+    #custom-properties-selete
+    expense_filter = ExpenseFilter(
+        condition = 'Kratos',
+        operator = 'not_in',
+        values = ['BOOK', 'Dev-D'],
+        rank = 1,
+        is_custom = True,
+        custom_field_type = 'SELECT'
+    )
+
+    constructed_expense_filter = construct_expense_filter(expense_filter)
+
+    filter_1 = {'custom_properties__Kratos__in': ['BOOK', 'Dev-D']}
+    response = ~Q(**filter_1)
+
+    assert constructed_expense_filter == response
+
 
 @pytest.mark.django_db()
 def test_multiple_construct_expense_filter():
