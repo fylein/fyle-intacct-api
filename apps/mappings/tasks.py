81a8f35aa253d85e6fc737dfd1fe32b8e30bb8bd
labhvam5 <labhvam.s@fyle.in>
2023-08-01 11:29:33 +0530

    reverting the tasks.py file

--- apps/mappings/tasks.py
+++ apps/mappings/tasks.py
@@ -1279,6 +1279,7 @@ def auto_import_and_map_fyle_fields(workspace_id):
     Auto import and map fyle fields
     """
     configuration: Configuration = Configuration.objects.get(workspace_id=workspace_id)
+    project_mapping = MappingSetting.objects.filter(source_field='PROJECT', workspace_id=configuration.workspace_id).first()
 
     chain = Chain()
 
@@ -1288,7 +1289,9 @@ def auto_import_and_map_fyle_fields(workspace_id):
     if configuration.import_categories:
         chain.append('apps.mappings.tasks.auto_create_category_mappings', workspace_id)
 
-    print(chain.length())
+    if project_mapping and project_mapping.import_to_fyle:
+        chain.append('apps.mappings.tasks.auto_create_project_mappings', workspace_id)
+
     if chain.length() > 0:
         chain.run()
 
