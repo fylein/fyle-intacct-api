8c79eccb3e864dbf905c34d6760e1faaddf7b33f
labhvam5 <labhvam.s@fyle.in>
2023-08-11 18:39:41 +0530

    removing auto_create_projects code and test

--- tests/test_mappings/test_signals.py
+++ tests/test_mappings/test_signals.py
@@ -151,27 +151,6 @@ def test_run_post_mapping_settings_triggers(db, mocker, test_connection):
 
     workspace_id = 1
 
-    mapping_setting = MappingSetting.objects.filter(source_field='PROJECT', destination_field='PROJECT').first()
-    mapping_setting.delete()
-
-    mapping_setting = MappingSetting(
-        source_field='PROJECT',
-        destination_field='PROJECT',
-        workspace_id=workspace_id,
-        import_to_fyle=True,
-        is_custom=False
-    )
-
-    mapping_setting.save()
-
-    schedule = Schedule.objects.filter(
-        func='apps.mappings.tasks.auto_create_project_mappings',
-        args='{}'.format(workspace_id),
-    ).first()
-
-    assert schedule.func == 'apps.mappings.tasks.auto_create_project_mappings'
-    assert schedule.args == '1'
-
     mapping_setting = MappingSetting(
         source_field='COST_CENTER',
         destination_field='CLASS',
