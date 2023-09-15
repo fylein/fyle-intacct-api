6abade84726f99d0527f67c1d4d373215a622af4
labhvam5 <88420539+labhvam5@users.noreply.github.com>
2023-09-15 16:33:54 +0530

    Revert "Revert "Import Categories Rework (#341)" (#348)" (#349)
    This reverts commit 40e334bd6e9773d1202e14e9f9f0f759e078f4ac.

--- apps/mappings/imports/schedules.py
+++ apps/mappings/imports/schedules.py
@@ -11,6 +11,7 @@ def schedule_or_delete_fyle_import_tasks(configuration: Configuration, mapping_s
     :param instance: Mapping Setting Instance
     :return: None
     """
+    task_to_be_scheduled = None
     # Check if there is a task to be scheduled
     if mapping_setting_instance:
         task_to_be_scheduled = MappingSetting.objects.filter(
@@ -19,18 +20,17 @@ def schedule_or_delete_fyle_import_tasks(configuration: Configuration, mapping_s
             source_field=mapping_setting_instance.source_field
         ).first()
 
-        if task_to_be_scheduled:
-            Schedule.objects.update_or_create(
-                func='apps.mappings.imports.queues.chain_import_fields_to_fyle',
-                args='{}'.format(configuration.workspace_id),
-                defaults={
-                    'schedule_type': Schedule.MINUTES,
-                    'minutes': 24 * 60,
-                    'next_run': datetime.now()
-                }
-            )
-            return
-
+    if task_to_be_scheduled or configuration.import_categories:
+        Schedule.objects.update_or_create(
+            func='apps.mappings.imports.queues.chain_import_fields_to_fyle',
+            args='{}'.format(configuration.workspace_id),
+            defaults={
+                'schedule_type': Schedule.MINUTES,
+                'minutes': 24 * 60,
+                'next_run': datetime.now()
+            }
+        )
+        return
 
     # If none of the conditions are met, delete the existing schedule
     Schedule.objects.filter(
