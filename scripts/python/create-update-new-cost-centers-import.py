239a507f40fe949a87f1872e618f57f700dd09a3
labhvam5 <88420539+labhvam5@users.noreply.github.com>
2023-09-22 11:03:14 +0530

    cost-centers import rewrite (#345)
    * Cost-centers import rewrite
    
    * new master
    
    * adding signals logic
    
    * adding test
    
    * adding test
    
    * removing old test cases
    
    * resolving lint
    
    * resolving comments
    
    * adding migration script
    
    * migration script

--- scripts/python/create-update-new-cost-centers-import.py
+++ scripts/python/create-update-new-cost-centers-import.py
@@ -4,13 +4,13 @@ from django_q.models import Schedule
 from apps.workspaces.models import Configuration
 from fyle_accounting_mappings.models import MappingSetting
 existing_import_enabled_schedules = Schedule.objects.filter(
-    func__in=['apps.mappings.tasks.auto_import_and_map_fyle_fields']
+    func__in=['apps.mappings.tasks.auto_create_cost_center_mappings']
 ).values('args')
 try:
     with transaction.atomic():
         for schedule in existing_import_enabled_schedules:
-            configuration = Configuration.objects.get(workspace_id=schedule['args'])
-            if configuration.import_categories:
+            mapping_setting = MappingSetting.objects.filter(source_field='COST_CENTER', workspace_id=schedule['args'], import_to_fyle=True).first()
+            if mapping_setting:
                 Schedule.objects.update_or_create(
                     func='apps.mappings.imports.queues.chain_import_fields_to_fyle',
                     args=schedule['args'],
@@ -22,10 +22,12 @@ try:
                 )
         categories = Configuration.objects.filter(import_categories=True).values_list('workspace_id', flat=True)
         project = MappingSetting.objects.filter(source_field='PROJECT', import_to_fyle=True).values_list('workspace_id', flat=True)
-        unique_workspace_ids = list(set(categories) | set(project))
-        total_count = len(unique_workspace_ids)
+        cost_center = MappingSetting.objects.filter(source_field='COST_CENTER', import_to_fyle=True).values_list('workspace_id', flat=True)
+        unique_states = list(set(categories) | set(project) | set(cost_center))
+        tot_count = len(unique_states)
         schedule_count = Schedule.objects.filter(func='apps.mappings.imports.queues.chain_import_fields_to_fyle').count()
-        print("total_count: {}".format(total_count))
+        #make the sanity check a bit more clear
+        print("tot_count: {}".format(tot_count))
         print("schedule_count: {}".format(schedule_count))
         raise Exception("This is a sanity check")
 except Exception as e:
