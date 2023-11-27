5da783ab7957cf00ba1a6c5f137538ce3ba3002e
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2023-11-27 12:03:34 +0530

    Set Last successful sync to null when import settings are changed (#396)

--- apps/workspaces/apis/import_settings/triggers.py
+++ apps/workspaces/apis/import_settings/triggers.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 from django.db.models import Q
 
 from apps.mappings.helpers import schedule_or_delete_fyle_import_tasks
+from apps.mappings.models import ImportLog
 from apps.workspaces.models import Configuration
 from fyle_accounting_mappings.models import MappingSetting
 
@@ -35,12 +36,23 @@ class ImportSettingsTrigger:
         for setting in self.__mapping_settings:
             if setting['destination_field'] not in destination_fields:
                 destination_fields.append(setting['destination_field'])
-        
-        #Now that we have all the system necessary mapping-settings and the mapping-settings in the payload
-        #This query will take care of deleting all the redundant mapping-settings that are not required.
-        MappingSetting.objects.filter(
+
+        # Now that we have all the system necessary mapping-settings and the mapping-settings in the payload
+        # This query will take care of deleting all the redundant mapping-settings that are not required.
+        mapping_settings_to_delete = MappingSetting.objects.filter(
             ~Q(destination_field__in=destination_fields),
             workspace_id=self.__workspace_id
-        ).delete()
+        ).all()
+
+        if mapping_settings_to_delete:
+            source_fields = [mapping_setting.source_field for mapping_setting in mapping_settings_to_delete]
+
+            # Delete the import logs for the redundant mapping-settings
+            ImportLog.objects.filter(
+                workspace_id=self.__workspace_id, attribute_type__in=source_fields
+            ).update(last_successful_run_at=None)
+
+            # Delete the mapping settings
+            mapping_settings_to_delete.delete()
 
         schedule_or_delete_fyle_import_tasks(configurations_instance)
