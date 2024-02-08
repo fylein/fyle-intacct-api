7c6986f311a388430d48b066729b41af612427c2
ruuushhh <rushikeshtayade2001@gmail.com>
2024-02-08 11:42:56 +0530

    Script modified

--- scripts/python/create-admin-subscriptions.py
+++ scripts/python/create-admin-subscriptions.py
@@ -2,12 +2,17 @@
 
 from apps.workspaces.tasks import async_create_admin_subcriptions
 from apps.workspaces.models import Workspace
+from fyle.platform.exceptions import InvalidTokenError as FyleInvalidTokenError
+
 
 workspaces = Workspace.objects.all()
 
 for workspace in workspaces:
     try:
         async_create_admin_subcriptions(workspace.id)
+        print('Admin subscriptions created for workspace - {} with ID - {}'.format(workspace.name, workspace.id))
+    except FyleInvalidTokenError as e:
+        print('Invalid Token for workspace - {} with ID - {}'.format(workspace.name, workspace.id))
     except Exception as e:
         print('Error while creating admin subscriptions for workspace - {} with ID - {}'.format(workspace.name, workspace.id))
         print(e.__dict__)
