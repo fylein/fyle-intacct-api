e18426aa28038822b6b555f24289341a993348af
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2024-03-05 15:17:56 +0530

    Fixing payment trigger

--- apps/sage_intacct/tasks.py
+++ apps/sage_intacct/tasks.py
@@ -741,6 +741,10 @@ def create_expense_report(expense_group: ExpenseGroup, task_log_id: int, last_ex
         task_log.save()
         update_failed_expenses(expense_group.expenses.all(), True)
         logger.exception('Something unexpected happened workspace_id: %s %s', task_log.workspace_id, task_log.detail)
+    
+    if last_export:
+        if configuration.sync_fyle_to_sage_intacct_payments:
+            create_sage_intacct_reimbursement(workspace_id=expense_group.workspace.id)
 
 
 def create_bill(expense_group: ExpenseGroup, task_log_id: int, last_export: bool):
@@ -854,6 +858,10 @@ def create_bill(expense_group: ExpenseGroup, task_log_id: int, last_export: bool
         task_log.save()
         update_failed_expenses(expense_group.expenses.all(), True)
         logger.exception('Something unexpected happened workspace_id: %s %s', task_log.workspace_id, task_log.detail)
+
+        if last_export:            
+            if configuration.sync_fyle_to_sage_intacct_payments:
+                create_ap_payment(workspace_id=expense_group.workspace.id)
 
 
 def create_charge_card_transaction(expense_group: ExpenseGroup, task_log_id: int, last_export: bool):
