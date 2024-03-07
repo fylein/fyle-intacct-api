10266522a1cac477ec8c2a5a197528f9846a74a9
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2024-03-07 14:57:33 +0530

    Add Error parse scripts (#448)

---
+++ scripts/python/remove-support-id-from-errors.py
@@ -0,0 +1,15 @@
+from apps.sage_intacct.errors.helpers import remove_support_id
+from apps.tasks.models import Error
+
+errors_count = Error.objects.filter(type='INTACCT_ERROR').count()
+
+page_size = 200
+for offset in range(0, errors_count, page_size):
+    limit = offset + page_size
+    paginated_errors = Error.objects.filter(type='INTACCT_ERROR').order_by('id')[offset:limit]
+
+    for error in paginated_errors:
+        err_msg = error.error_detail
+        err_msg = remove_support_id(err_msg)
+        error.error_detail = err_msg
+        error.save()
