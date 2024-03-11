77cf11880e1ca66f79e9645c223066c86d230ce7
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-03-11 12:03:32 +0530

    Revert "Fix: Scripts Parse Error (#456)"
    This reverts commit 7f9d04542aa73f221d1b29b4065819763808ed2e.

--- scripts/python/remove-support-id-from-errors.py
+++ scripts/python/remove-support-id-from-errors.py
@@ -2,9 +2,7 @@ from apps.sage_intacct.errors.helpers import remove_support_id
 from apps.tasks.models import Error
 
 errors_count = Error.objects.filter(type='INTACCT_ERROR').count()
-print(errors_count)
 
-count = 0
 page_size = 200
 for offset in range(0, errors_count, page_size):
     limit = offset + page_size
@@ -15,5 +13,3 @@ for offset in range(0, errors_count, page_size):
         err_msg = remove_support_id(err_msg)
         error.error_detail = err_msg
         error.save()
-        count += 1
-print(count)
