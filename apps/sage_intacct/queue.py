f872a866521b835ac24da10dedc45c6f2f83f158
ruuushhh <rushikeshtayade2001@gmail.com>
2024-03-22 17:53:51 +0530

    Bump platform connector, accounting mappings, export sync modified

--- apps/sage_intacct/queue.py
+++ apps/sage_intacct/queue.py
@@ -30,7 +30,7 @@ def __create_chain_and_run(fyle_credentials: FyleCredential, in_progress_expense
     chain = Chain()
 
     chain.append('apps.sage_intacct.tasks.update_expense_and_post_summary', in_progress_expenses, workspace_id, fund_source)
-    chain.append('apps.fyle.helpers.sync_dimensions', fyle_credentials, workspace_id)
+    chain.append('apps.fyle.helpers.sync_dimensions', fyle_credentials, True)
     chain.append('apps.fyle.tasks.sync_reimbursements', fyle_credentials, workspace_id)
 
     for task in chain_tasks:
