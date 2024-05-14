166cad4408ebd02c5e929f026d9816eedacb3742
anishfyle <anishkumar.s@fyle.in>
2024-05-14 14:12:38 +0530

    Fixed category

--- tests/test_fyle/test_helpers.py
+++ tests/test_fyle/test_helpers.py
@@ -209,7 +209,7 @@ def test_construct_expense_filter():
     )
     constructed_expense_filter = construct_expense_filter(expense_filter)
 
-    filter_1 = {'custom_properties__category__in':['anish', 'singh']}
+    filter_1 = {'category__in':['anish', 'singh']}
     response = ~Q(**filter_1)
 
     assert constructed_expense_filter == response
