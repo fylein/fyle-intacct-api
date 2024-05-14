c3d77f07cd357f32b95af560e159bf20bcc2e18c
anishfyle <anishkumar.s@fyle.in>
2024-05-14 10:45:01 +0530

    category added as default condition

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -422,12 +422,6 @@ class CustomFieldView(generics.RetrieveAPIView):
                     'is_custom': custom_field['is_custom']
                 })
 
-        response.append({
-                'field_name': 'CATEGORY', 
-                'type': 'SELECT', 
-                'is_custom': 'true'
-            })
-            
         return Response(
             data=response,
             status=status.HTTP_200_OK
