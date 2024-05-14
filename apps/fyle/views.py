185b903bdf85d43cfaf7c291ef2098b886e371c0
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2024-05-14 14:21:33 +0530

    Support for Category Condition in Skip Export (#489)
    * Support for Category Condition in Skip Export
    
    * updated views for category skip export
    
    * Fix Intacct Skip Export
    
    * Fix for category support skip export
    
    * category added as default condition
    
    * Support added for not_in
    
    * Fixed minor bug
    
    * added tests asserts for few cases for category
    
    * few minor fixes
    
    * fixed tests
    
    * Fixed category
    
    * remove loggers

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -421,7 +421,7 @@ class CustomFieldView(generics.RetrieveAPIView):
                     'type': custom_field['type'],
                     'is_custom': custom_field['is_custom']
                 })
-            
+
         return Response(
             data=response,
             status=status.HTTP_200_OK
