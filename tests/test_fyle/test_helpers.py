e6c535e6a0bd903a2f155f1c7d6f421f344e57a6
anishfyle <anishkumar.s@fyle.in>
2024-05-14 13:02:45 +0530

    few minor fixes

--- tests/test_fyle/test_helpers.py
+++ tests/test_fyle/test_helpers.py
@@ -195,7 +195,7 @@ def test_construct_expense_filter():
     )
     constructed_expense_filter = construct_expense_filter(expense_filter)
 
-    filter_1 = {'category__iexact':'anish'}
+    filter_1 = {'category__iexact':['anish']}
     response = Q(**filter_1)
 
     assert constructed_expense_filter == response
@@ -209,7 +209,7 @@ def test_construct_expense_filter():
     )
     constructed_expense_filter = construct_expense_filter(expense_filter)
 
-    filter_1 = {'category__in':'anish'}
+    filter_1 = {'category__in':['anish']}
     response = ~Q(**filter_1)
 
     assert constructed_expense_filter == response
