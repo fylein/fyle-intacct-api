b444e709160a44a3f8ae7720ab508595c98cbc20
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-06-14 15:05:12 +0530

    Remove old schedule

--- apps/sage_intacct/dependent_fields.py
+++ apps/sage_intacct/dependent_fields.py
@@ -171,24 +171,3 @@ def create_dependent_custom_field_in_fyle(workspace_id: int, fyle_attribute_type
 
     return platform.expense_custom_fields.post(expense_custom_field_payload)
 
-
-def schedule_dependent_field_imports(workspace_id: int, is_import_enabled: bool):
-    if is_import_enabled:
-        Schedule.objects.update_or_create(
-            func='apps.sage_intacct.dependent_fields.import_dependent_fields_to_fyle',
-            cluster='import',
-            args='{}'.format(workspace_id),
-            defaults={
-                'schedule_type': Schedule.MINUTES,
-                'minutes': 24 * 60,
-                'next_run': datetime.now()
-            }
-        )
-    else:
-        schedule: Schedule = Schedule.objects.filter(
-            func='apps.sage_intacct.dependent_fields.import_dependent_fields_to_fyle',
-            args='{}'.format(workspace_id)
-        ).first()
-
-        if schedule:
-            schedule.delete()
