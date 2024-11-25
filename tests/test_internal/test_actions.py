208e636af0536fa57ae5ae672ce5245fd23ac2af
ashwin1111 <winash1998@gmail.com>
2024-11-25 14:25:56 +0530

    feat: Add support for internal APIs

---
+++ tests/test_internal/test_actions.py
@@ -0,0 +1,31 @@
+from apps.internal.actions import get_accounting_fields, get_exported_entry
+from tests.test_sageintacct.fixtures import data
+
+
+def test_get_accounting_fields(db, mocker):
+    query_params = {
+        'org_id': 'or79Cob97KSh',
+        'resource_type': 'employees',
+    }
+    mocker.patch(
+        'sageintacctsdk.apis.Employees.get_all_generator',
+        return_value=data['get_employees']
+    )
+
+    fields = get_accounting_fields(query_params)
+    assert fields is not None
+
+
+def test_get_exported_entry(db, mocker):
+    query_params = {
+        'org_id': 'or79Cob97KSh',
+        'resource_type': 'bill',
+        'internal_id': '1'
+    }
+    mocker.patch(
+        'sageintacctsdk.apis.Bills.get',
+        return_value={'summa': 'hehe'}
+    )
+
+    entry = get_exported_entry(query_params)
+    assert entry is not None
