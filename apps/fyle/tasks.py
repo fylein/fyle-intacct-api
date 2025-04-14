1a288b6b2382a4ad9a4070420cfd148bdc0829f0
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2025-04-14 23:20:35 +0530

    fix: skipped direct export sync (#665)
    * fix: skipped direct export sync
    
    * Fix flake;

--- apps/fyle/tasks.py
+++ apps/fyle/tasks.py
@@ -1,6 +1,6 @@
 import logging
 import traceback
-from datetime import datetime, timezone
+from datetime import datetime
 
 from django.db import transaction
 from django_q.tasks import async_task
@@ -93,7 +93,6 @@ def create_expense_groups(workspace_id: int, fund_source: list[str], task_log: T
     :param workspace_id: workspace id
     :param fund_source: expense fund source
     """
-    configuration = Configuration.objects.get(workspace_id=workspace_id)
     try:
         with transaction.atomic():
             workspace = Workspace.objects.get(pk=workspace_id)
@@ -143,34 +142,7 @@ def create_expense_groups(workspace_id: int, fund_source: list[str], task_log: T
 
             workspace.save()
 
-            expense_objects = Expense.create_expense_objects(expenses, workspace_id, imported_from=imported_from)
-
-            expense_filters = ExpenseFilter.objects.filter(workspace_id=workspace_id).order_by('rank')
-            filtered_expenses = expense_objects
-            if expense_filters:
-                expenses_object_ids = [expense_object.id for expense_object in expense_objects]
-                final_query = construct_expense_filter_query(expense_filters)
-                Expense.objects.filter(
-                    final_query,
-                    id__in=expenses_object_ids,
-                    expensegroup__isnull=True,
-                    org_id=workspace.fyle_org_id
-                ).update(is_skipped=True, updated_at=datetime.now(timezone.utc))
-
-                filtered_expenses = Expense.objects.filter(
-                    is_skipped=False,
-                    id__in=expenses_object_ids,
-                    expensegroup__isnull=True,
-                    org_id=workspace.fyle_org_id)
-
-            ExpenseGroup.create_expense_groups_by_report_id_fund_source(
-                filtered_expenses,
-                configuration,
-                workspace_id
-            )
-
-            task_log.status = 'COMPLETE'
-            task_log.save()
+            group_expenses_and_save(expenses, task_log, workspace, imported_from=imported_from)
 
     except NoPrivilegeError:
         logger.info('Invalid Fyle Credentials / Admin is disabled')
