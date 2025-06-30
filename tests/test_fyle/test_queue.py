791825cace043bf504be65c431a3287678e680da
ashwin1111 <winash1998@gmail.com>
2025-06-30 20:57:03 +0530

    fix import

--- tests/test_fyle/test_queue.py
+++ tests/test_fyle/test_queue.py
@@ -1,18 +1,22 @@
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta, timezone
 
 from fyle_accounting_library.rabbitmq.data_class import Task
 
 from apps.fyle.models import ExpenseGroup
+from apps.fyle.queue import async_import_and_export_expenses
+from apps.sage_intacct.queue import __create_chain_and_run, validate_failing_export
 from apps.tasks.models import TaskLog
 from apps.workspaces.models import Workspace
-from apps.sage_intacct.queue import __create_chain_and_run, validate_failing_export
-from apps.fyle.queue import async_import_and_export_expenses
 
 
-def test_create_chain_and_run(db):
+def test_create_chain_and_run(db, mocker):
     """
     Test create_chain_and_run
     """
+    mock_check_interval = mocker.patch('apps.sage_intacct.queue.check_interval_and_sync_dimension')
+
+    mock_task_executor_run = mocker.patch('fyle_accounting_library.rabbitmq.helpers.TaskChainRunner.run')
+
     workspace_id = 1
     chain_tasks = [
         Task(
@@ -21,8 +25,10 @@ def test_create_chain_and_run(db):
         )
     ]
 
-    __create_chain_and_run(workspace_id, chain_tasks, False)
-    assert True
+    __create_chain_and_run(workspace_id, chain_tasks, True)
+
+    mock_check_interval.assert_called_once_with(workspace_id)
+    mock_task_executor_run.assert_called_once_with(chain_tasks, workspace_id)
 
 
 def test_async_import_and_export_expenses(db):
