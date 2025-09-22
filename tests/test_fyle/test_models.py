b4c332865e673cd9f71a966895bb1a6534468619
Hrishabh Tiwari <74908943+Hrishabh17@users.noreply.github.com>
2025-09-22 13:28:53 +0530

    feat: handle fund_source change for expenses (#759)
    * feat: handle fund_source change for expenses
    
    * add tests
    
    * fix: edge cases
    
    * add tests
    
    * flake
    
    * added fixture
    
    * flake resolved
    
    * fix test
    
    * added test to all lines
    
    * added tests for lines
    
    * decrease cov for now
    
    * fix test
    
    ---------
    
    Co-authored-by: Ashutosh619-sudo <ashutosh.s@fyle.com>

--- tests/test_fyle/test_models.py
+++ tests/test_fyle/test_models.py
@@ -54,14 +54,14 @@ def test_create_eliminated_expense_objects(db):
     assert len(expense) == 1
 
 
-def test_expense_group_settings(create_temp_workspace, db):
+def test_expense_group_settings(db):
     """
     Test expense group settings
     """
-    workspace_id = 98
+    workspace_id = 1
     payload = data['expense_group_settings_payload']
 
-    user = Workspace.objects.get(id=1).user
+    user = Workspace.objects.get(id=workspace_id).user
 
     ExpenseGroupSettings.update_expense_group_settings(
         payload, workspace_id, user
