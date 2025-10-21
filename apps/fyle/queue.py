d2e821f45d939558836f08b98802d61678c91ac2
Ashutosh619-sudo <ashutosh.s@fyle.com>
2025-10-21 12:02:01 +0530

    comment resolved

--- apps/fyle/queue.py
+++ apps/fyle/queue.py
@@ -15,11 +15,14 @@ def async_import_and_export_expenses(body: dict, workspace_id: int) -> None:
     :param body: body
     :return: None
     """
+    if body.get('data') and body['data'].get('org_id'):
+        org_id = body['data']['org_id']
+        assert_valid_request(workspace_id=workspace_id, fyle_org_id=org_id)
+
     if body.get('action') in ('ADMIN_APPROVED', 'APPROVED', 'STATE_CHANGE_PAYMENT_PROCESSING', 'PAID') and body.get('data'):
         report_id = body['data']['id']
         org_id = body['data']['org_id']
         state = body['data']['state']
-        assert_valid_request(workspace_id=workspace_id, fyle_org_id=org_id)
         payload = {
             'workspace_id': workspace_id,
             'action': WorkerActionEnum.EXPENSE_STATE_CHANGE.value,
@@ -36,7 +39,6 @@ def async_import_and_export_expenses(body: dict, workspace_id: int) -> None:
     elif body.get('action') == 'ACCOUNTING_EXPORT_INITIATED' and body.get('data'):
         report_id = body['data']['id']
         org_id = body['data']['org_id']
-        assert_valid_request(workspace_id=workspace_id, fyle_org_id=org_id)
         payload = {
             'workspace_id': workspace_id,
             'action': WorkerActionEnum.DIRECT_EXPORT.value,
@@ -53,7 +55,6 @@ def async_import_and_export_expenses(body: dict, workspace_id: int) -> None:
     elif body.get('action') == 'UPDATED_AFTER_APPROVAL' and body.get('data') and body.get('resource') == 'EXPENSE':
         org_id = body['data']['org_id']
         logger.info("| Updating non-exported expenses through webhook | Content: {{WORKSPACE_ID: {} Payload: {}}}".format(workspace_id, body.get('data')))
-        assert_valid_request(workspace_id=workspace_id, fyle_org_id=org_id)
         payload = {
             'workspace_id': workspace_id,
             'action': WorkerActionEnum.EXPENSE_UPDATED_AFTER_APPROVAL.value,
@@ -68,7 +69,6 @@ def async_import_and_export_expenses(body: dict, workspace_id: int) -> None:
         expense_id = body['data']['id']
         action_type = body.get('action')
         logger.info("| Handling expense %s | Content: {WORKSPACE_ID: %s EXPENSE_ID: %s Payload: %s}", action_type.lower().replace('_', ' '), workspace_id, expense_id, body.get('data'))
-        assert_valid_request(workspace_id=workspace_id, fyle_org_id=org_id)
         payload = {
             'workspace_id': workspace_id,
             'action': WorkerActionEnum.EXPENSE_ADDED_EJECTED_FROM_REPORT.value,
